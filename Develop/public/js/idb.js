a2a8e655bd02205e982a2cd77af16efd6d9586f6
nlopez <nelly.lopez@utexas.edu>
2021-03-04 20:11:41 -0600

    updated idb and service-worker files

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -3,20 +3,26 @@ const indexedDB = window.indexedDB
 let db;
 const request = indexedDB.open('budget-tracker', 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    db.createObjectStore('new_transaction', {autoIncrement: true});
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 }
 
-request.onerror = function(event) {
+request.onerror = function (event) {
     console.log(event.target.errorCode);
 }
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
-    if(navigator.onLine) {
+    if (navigator.onLine) {
         uploadTransaction();
     }
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const store = transaction.objectStore('new_transaction');
+    store.add(record);
 }
 
 function uploadTransaction() {
@@ -24,24 +30,33 @@ function uploadTransaction() {
     const store = transaction.objectStore('new_transaction');
     const getAll = store.getAll();
 
-getAll.onsuccess = function() {
-    if(getAll.result.length > 0) {
-        fetch('/api/transaction/bulk', {
-            method: 'POST',
-            body: JSON.stringify(getAll.result),
-            headers: {
-                Accept: 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(() => {
-            const transaction = db.transaction(['new_transaction'], 'readwrite');
-            const store = transaction.objectStore('new_transaction');
-            store.clear()
-        })
-        //taking data in db and deleting/clearing it out
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((serverResponse) => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse)
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const store = transaction.objectStore('new_transaction');
+                    store.clear();
+
+                    alert('All transactions that were saved have been submitted.')
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+            //taking data in db and deleting/clearing it out
+        }
     }
 }
-}
 
+window.addEventListener('online', uploadTransaction);
